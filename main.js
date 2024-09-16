d09d493ef7765b9a9fd3fd100b666c1722445aa6
Abhijeet Chatterjee <97887115+Abhijxxt@users.noreply.github.com>
2024-09-16 14:21:57 +0530

    Added audio playback and audio functions working buttons

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ const text = document.getElementById('text')
 let first = 0;
 let word = '';
 
+const dotAudio = new Audio('audios/dot.mp3');
+const dashAudio = new Audio('audios/dash.mp3');
+
+
 let letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
     'U', 'V', 'W', 'X', 'Y', 'Z'
 ];
@@ -11,6 +15,42 @@ let morse = ['._','_...','_._.','_..','.','.._.','__.','....','..','.___','_._',
     '.__.','__._','._.','...','_','.._','..._','.__','_.._','_.__','__..'
 ];
 
+document.getElementById('clear').addEventListener('click', () => {
+    console.log('clicking');
+    editor.textContent = 'Press or Hold Space to write Morse Code';
+    text.textContent = 'Text would be written here';
+    word = '';
+    first = 0;
+    document.getElementById('clear').disabled = true;
+})
+
+
+function playSound(letter) {
+    if(letter == '.') {
+        dotAudio.play();
+        // await sleep(1000);
+    }
+    if(letter == '_') {
+        dashAudio.play();
+        // await sleep(1000);
+        // dotAudio.addEventListener('ended', () => {return})
+    }
+}
+
+document.getElementById('speak-morse').addEventListener('click', () => {
+    let code = editor.textContent;
+    let i = 0;
+    var audioplayerID = setInterval(()=> {
+        if(i<code.length) {
+            console.log(code[i]);
+            playSound(code[i]);
+            i++;
+        } else {
+            clearInterval(audioplayerID);
+        }
+    }, 100);
+    }
+)
 
 document.getElementById('copy-morse').addEventListener('click', () => {
     navigator.clipboard.writeText(editor.textContent);
@@ -23,6 +63,7 @@ function converter() {
 }
 
 document.onkeydown = (e) => {
+    document.getElementById('clear').disabled = false;
     // console.log(e.key);
     if (first == 0) {
         editor.textContent = '';
@@ -41,18 +82,15 @@ document.onkeyup = (e) => {
         word = '';
     }
     if(repeat && e.key == ' ') {
-        console.log("_");
+        // console.log("_");
         editor.textContent = editor.textContent + '_ ';
+        dashAudio.play();
         word += "_";
         repeat = !repeat; 
     }else if (!repeat && e.key == ' '){
-        console.log(".");
+        // console.log(".");
+        dotAudio.play();
         word += ".";
         editor.textContent = editor.textContent + '. ';
     }
 }
-
-document.getElementById('clear').addEventListener('click', () => {
-    editor.textContent = 'Press or Hold Space to write Morse Code';
-    text.textContent = 'Text would be written here';
-})
